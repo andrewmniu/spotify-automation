d8e4a5ffd419b7711f17e06811e25e1b1c0dee78
andrewmniu <andrewmniu@gmail.com>
2020-06-23 14:37:33 -0400

    changed to cron

--- month_playlists.py
+++ month_playlists.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', minutes=5)
+@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=15)
 def main():
     today = date.today()
     first = today.replace(day=1)
